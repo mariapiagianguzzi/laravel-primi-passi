977bd8831ca1a99283fa84037057ae79408d05f2
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-02-10 16:42:40 +0100

    laravel.primi-passi

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-<a href="{{ url('/') }}">ritorna</a>
+    <main>
+            <div class="container">
+            <h3>Melagrana Handmade</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum neque officiis est culpa maxime amet, quia quibusdam cumque consequuntur, exercitationem ipsum, voluptatum reprehenderit illum asperiores? Eaque tenetur dicta labore eius.</p>
+            </div>
+    </main>
+    <a href="{{ url('/') }}">ritorna</a>
     
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,70 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        
+        <style>
+            *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            }
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+                padding: 20px;
+            }
+            .container {
+                width: 1070px;
+                height: 100%;
+                margin: 0 auto;
+            }
+
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
     </head>
     <body>
             <header>
@@ -18,18 +81,43 @@
                     <a href="{{ url('/') }}">Home</a>
                 </nav>
             </header>
-            <main>
-            <div class="container">
-            <h3>Melagrana Handmade</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum neque officiis est culpa maxime amet, quia quibusdam cumque consequuntur, exercitationem ipsum, voluptatum reprehenderit illum asperiores? Eaque tenetur dicta labore eius.</p>
-            </div>
-            </main>
             <footer>
                 <div class="container">
                 <a href="{{ url('/about') }}">About</a>
-                <a href="{{ url('/about') }}">Contact</a>
                 <a href="mailto:text@gmail.com">Email</a>
                 </div>
             </footer>
+         <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
